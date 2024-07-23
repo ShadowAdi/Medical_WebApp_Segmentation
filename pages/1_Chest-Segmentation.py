3389a3d67ba19fec3ba513e5434935fad45a5025
Aditya Shukla <Shadowshukla76@gmail.com>
2024-07-23 10:00:08 +0530

    Update 1_Chest-Segmentation.py

--- pages/1_Chest-Segmentation.py
+++ pages/1_Chest-Segmentation.py
@@ -24,19 +24,22 @@ img=st.file_uploader("Upload an image of your chest", type=["png", "jpg", "jpeg"
 if img is not None:
     input_image=chest_segmentation(img)
     prediction=get_chest_model(input_image=input_image)
-    predicted_image = Image.fromarray(prediction.squeeze(), mode='L')
-    img_bytes=download_image(predicted_image=predicted_image)
+    try:
+        predicted_image = Image.fromarray(prediction.squeeze(), mode='L')
+        img_bytes=download_image(predicted_image=predicted_image)
+        st.image(img, caption="Original Image", use_column_width=True)
+        st.image(input_image, caption="Image Augmented Image", use_column_width=True)
+        st.image(prediction.squeeze(), caption="Segmented Image", use_column_width=True)
 
-    st.image(img, caption="Original Image", use_column_width=True)
-    st.image(input_image, caption="Image Augmented Image", use_column_width=True)
-    st.image(prediction.squeeze(), caption="Segmented Image", use_column_width=True)
+        st.download_button(
+            label="Download Segmented Image",
+            data=img_bytes,
+            file_name="segmented_image.png",
+            mime="image/png"
+        )
+    except:
+        st.error(prediction)
 
-    st.download_button(
-        label="Download Segmented Image",
-        data=img_bytes,
-        file_name="segmented_image.png",
-        mime="image/png"
-    )
 
 
 
