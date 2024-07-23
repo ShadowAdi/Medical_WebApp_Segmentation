61bae1c0bd7a1f327ee38738094a359f50003a80
Aditya Shukla <Shadowshukla76@gmail.com>
2024-07-23 08:50:45 +0530

    changes made added demo video

--- utils/Download_Image.py
+++
@@ -1,7 +0,0 @@
-from io import BytesIO
-def download_image(predicted_image):
-    buffered = BytesIO()
-    predicted_image.save(buffered, format="PNG")
-    img_bytes = buffered.getvalue()
-    return img_bytes
-
